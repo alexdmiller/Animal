cebba2c4d442f1a26fae7c25bbbec3bb28bfd974
Alex Miller <alexmiller@gmail.com>
2013-04-20 13:56:49 -0700

    added compile script for tests

--- src/com/animalsplaygames/animal/test/events/EventEmitterTest.hx
+++ src/com/animalsplaygames/animal/test/events/EventEmitterTest.hx
@@ -1,10 +1,12 @@
 package com.animalsplaygames.animal.test.events;
 
+import  com.animalsplaygames.animal.events.EventEmitter;
+
 class EventEmitterTest extends haxe.unit.TestCase {
   public function testSingleEventRegistration() {
     var e : EventEmitter = new EventEmitter();
     var value : Int = 0;
-    var responseFunction = function() {
+    var responseFunction = function(event) {
       value = 1;
     };
     e.on('event', responseFunction);
