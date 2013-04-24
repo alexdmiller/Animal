c2ee4bda0c7012ee9953d425eefe00295e6238ce
Alex Miller <alexmiller@gmail.com>
2013-04-23 22:59:56 -0700

    deleted old testing files

--- src/com/animalsplaygames/animal/test/TestMain.hx
+++
@@ -1,9 +0,0 @@
-package com.animalsplaygames.animal.test;
-
-class TestMain {
-  static function main() {
-    var r = new haxe.unit.TestRunner();
-    r.add(new com.animalsplaygames.animal.test.events.EventEmitterTest());
-    r.run();
-  }
-}
