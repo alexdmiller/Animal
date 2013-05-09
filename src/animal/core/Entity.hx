8a76eea29ffe93d2310b757596c763cf676d0002
Alex Miller <alexmiller@gmail.com>
2013-05-09 13:35:00 -0700

    removing and adding components

--- src/animal/core/Entity.hx
+++ src/animal/core/Entity.hx
@@ -14,11 +14,37 @@ class Entity extends EventEmitter {
 
   // dispatch event
   public function addComponent(c : Component) : Void {
+    var key : String = Type.getClassName(Type.getClass(c));
+    if (hasComponentWithName(key)) {
+      throw "Cannot add component of type " + key + " because entity already has a component of this type.";
+    }
+    components.set(key, c);
+    dispatch('component_added', {
+      component: c
+    });
+  }
 
+  public function hasComponentWithName(componentName : String) {
+    return components.exists(componentName);
+  }
+
+  public function getComponentWithName(componentName : String) {
+    return components.get(componentName);
   }
 
   // dispatch event
   public function removeComponent(c : Component) : Void {
+    var key : String = Type.getClassName(Type.getClass(c));
+    removeComponentWithName(key);
+  }
 
+  public function removeComponentWithName(componentName : String) {
+    if (components.exists(componentName)) {
+      var c : Component = components.get(componentName);
+      components.remove(componentName);
+      dispatch('component_removed', {
+        component: c
+      });
+    }
   }
 }
--- test/EntityTest.hx
+++ test/EntityTest.hx
@@ -4,8 +4,45 @@ import massive.munit.Assert;
 import animal.core.Entity;
 
 class EntityTest extends haxe.unit.TestCase {
+  var e : Entity;
+
+  @Before
+  override public function setup() : Void {
+    e = new Entity(1);
+  }
+
   @Test
-  public function constructedEntity() {
-    var e : Entity = new Entity(1);
+  public function constructed_entity_has_ID() {
+    var e2 : Entity = new Entity(1);
+    Assert.areEqual(1, e2.id);
+  }
+
+  @Test
+  public function addComponent_hasComponent() {
+    var c = new DummyComponent();
+    var name : String = Type.getClassName(Type.getClass(c));
+    e.addComponent(c);
+    Assert.isTrue(e.hasComponentWithName(name));
+    Assert.areEqual(c, e.getComponentWithName(name));
+  }
+
+  @Test
+  public function addComponent_dispatches_event() {
+    var c = new DummyComponent();
+    e.on('component_added', function(event) {
+      Assert.isTrue(true);
+    });
+    e.addComponent(c);
+  }
+
+  @Test
+  public function removeComponent_dispatches_event() {
+    var c = new DummyComponent();
+    var name : String = Type.getClassName(Type.getClass(c));
+    e.on('component_removed', function(event) {
+      Assert.isTrue(true);
+    });
+    e.addComponent(c);
+    e.removeComponentWithName(name);
   }
 }
