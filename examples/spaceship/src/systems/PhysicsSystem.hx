6730a154a7e3cf35a939cdc8451c270e5d94b6b5
Alex Miller <alexmiller@gmail.com>
2013-05-17 13:25:11 -0700

    created system/component dirs

---
+++ examples/spaceship/src/systems/PhysicsSystem.hx
@@ -0,0 +1,13 @@
+package;
+
+import animal.core.System;
+
+class PhysicsSystem extends System {
+  public function new() {
+    super([]);
+  }
+
+  override public function update(delta : Int) : Void {
+    
+  }
+}
